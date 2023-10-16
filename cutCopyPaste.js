4b91ae1c40ba75429b4449a6ebb104a734518647
Sayandeep Paul <sayandeep775@gmail.com>
2023-10-17 01:59:32 +0530

    cut copy paste feature

--- cutCopyPaste.js
+++ cutCopyPaste.js
@@ -17,21 +17,21 @@ for (let i = 0; i < rows; i++) {
 
 let rangeStorage = []
 function handleSelectedCells(cell) {
-    cell.addEventListener("click",(e)=>{
+    cell.addEventListener("click", (e) => {
         // select cells range work
-        if(!ctrlKey) return
-        if(rangeStorage.length>=2) {
+        if (!ctrlKey) return
+        if (rangeStorage.length >= 2) {
             defaultCellsUI()
-            rangeStorage=[]
+            rangeStorage = []
         }
 
         // UI
         cell.style.outline = "none"
         cell.style.border = "3px solid #107c41"
 
-        let rid = Number(cell.getAttribute("rid")) 
+        let rid = Number(cell.getAttribute("rid"))
         let cid = Number(cell.getAttribute("cid"))
-        rangeStorage.push([rid,cid])
+        rangeStorage.push([rid, cid])
         console.log(rangeStorage)
     })
 }
@@ -42,3 +42,34 @@ function defaultCellsUI() {
         cell.style.border = "1px solid lightgrey"
     }
 }
+
+let copyBtn = document.querySelector(".copy")
+let cutBtn = document.querySelector(".cut")
+let pasteBtn = document.querySelector(".paste")
+
+let copyData = []
+copyBtn.addEventListener("click", (e) => {
+    let stRow = rangeStorage[0][0]
+    let stCol = rangeStorage[0][1]
+    let endRow = rangeStorage[1][0]
+    let endCol = rangeStorage[1][1]
+
+    for (let i = stRow; i <= endRow; i++) {
+        let copyRow = []
+        for (let j = stCol; j <= endCol; j++) {
+            let cellProp = sheetDB[i][j]
+            copyRow.push(cellProp)
+        }
+        copyData.push(copyRow)
+    }
+    defaultCellsUI()
+
+})
+
+cutBtn.addEventListener("click", (e) => {
+
+})
+
+pasteBtn.addEventListener("click", (e) => {
+
+})
