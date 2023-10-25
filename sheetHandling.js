a7a8f58f4bcb38d5eadfa418b74aa1f0c98e2437
Sayandeep Paul <sayandeep775@gmail.com>
2023-10-25 11:39:34 +0530

    sheets handling

--- sheetHandling.js
+++ sheetHandling.js
@@ -12,6 +12,7 @@ addSheetBtn.addEventListener("click", (e) => {
         <div class="sheet-content">Sheet ${allSheetFolders.length + 1}</div>
     `;
     sheetsFolderCont.appendChild(sheet);
+    sheet.scrollIntoView()
 
     createDB()
     createGraphcomponentMatrix()
