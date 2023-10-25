7f34070c749b33ce4aca1be7b53237f3063d7f0f
Sayandeep Paul <sayandeep775@gmail.com>
2023-10-25 11:39:49 +0530

    download and upload

---
+++ uploadAndDownload.js
@@ -0,0 +1,93 @@
+// let downloadBtn = document.querySelector(".download")
+
+// let openBtn = document.querySelector(".upload")
+
+// downloadBtn.addEventListener("click", (e) => {
+//     //Convert JSON Array to string.
+//     let jsonData = JSON.stringify([sheetDB, graphComponentMatrix]);
+//     //Convert JSON string to BLOB.
+//     let file = new Blob([jsonData], { type: "application/json" });
+
+//     let a = document.createElement("a");
+//     a.href = URL.createObjectURL(file);
+//     a.download = "SheetData.json";
+//     a.click();
+// })
+
+// // Open Task (upload)
+// openBtn.addEventListener("click", (e) => {
+//     // Create a file input element
+//     let input = document.createElement("input");
+//     input.setAttribute("type", "file");
+//     input.click();
+
+//     input.addEventListener("change", (e) => {
+//         let fr = new FileReader();
+//         let files = input.files
+//         let fileObj = files[0]
+
+//         fr.readAsText(fileObj);
+//         fr.addEventListener("load", (e) => {
+//             let readSheetData = JSON.parse(fr.result)
+
+//             // Basic sheet with default data will be created
+//             addSheetBtn.click()
+
+//             // sheetDB,graphComponentmatrix
+//             sheetDB = readSheetData[0]
+//             graphComponentMatrix = readSheetData[1]
+
+//             collectedSheetDB[collectedSheetDB.length - 1] = sheetDB
+//             collectedSheetDB[collectedSheetDB.length - 1] = graphComponentMatrix
+//             handleSheetProperties()
+//         })
+//     })
+// })
+
+
+
+
+let downloadBtn = document.querySelector(".download");
+let openBtn = document.querySelector(".upload");
+
+// Download Task
+downloadBtn.addEventListener("click", (e) => {
+    let jsonData = JSON.stringify([sheetDB, graphComponentMatrix]);
+    let file = new Blob([jsonData], { type: "application/json" });
+
+    let a = document.createElement("a");
+    a.href = URL.createObjectURL(file);
+    a.download = "SheetData.json";
+    a.click();
+})
+
+// Open task (upload)
+openBtn.addEventListener("click", (e) => {
+    // Opens file explorer
+    let input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.click();
+
+    input.addEventListener("change", (e) => {
+        let fr = new FileReader();
+        let files = input.files;
+        let fileObj = files[0];
+
+        fr.readAsText(fileObj);
+        fr.addEventListener("load", (e) => {
+            let readSheetData = JSON.parse(fr.result);
+
+            // Basic sheet with default data will be created
+            addSheetBtn.click();
+
+            // SheetDB, graphComponent
+            sheetDB = readSheetData[0];
+            graphComponentMatrix = readSheetData[1];
+            console.log("first")
+            collectedSheetDB[collectedSheetDB.length - 1] = sheetDB;
+            collectedSheetDB[collectedSheetDB.length - 1] = graphComponentMatrix;
+
+            handleSheetProperties();
+        })
+    })
+})
